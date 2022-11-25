ccfe9bca8cb2a5c5c2827f648d1cc9dc561323d8
John Castillo <john.gnu@gmail.com>
2022-11-25 13:59:10 -0400

    ff

--- arbol-hl/src/ejer1/Main.java
+++ arbol-hl/src/ejer1/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         ArbolP a = new ArbolP();
+        a.crear();
+        a.niveles();
         
+        System.out.println("\n Mostrar las hojas del nivel K");    
+        
+        System.out.println("\n Mostrar el pais con mayor extension del sub arbol derecho");
+                
+        System.out.println("\n Contar los paises del continente X");
     }
 }
