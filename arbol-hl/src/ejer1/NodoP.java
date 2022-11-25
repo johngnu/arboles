46e3fd828a9d0f5f7d1afb8d370f5ccafcc11677
John Castillo <john.gnu@gmail.com>
2022-11-25 12:54:41 -0400

    i2

--- arbol-hl/src/ejer1/NodoP.java
+++ arbol-hl/src/ejer1/NodoP.java
@@ -2,28 +2,19 @@ package ejer1;
 
 public class NodoP {
 
-    private String nombre;
-    private int edad;
+    private int ext;
     private NodoP der, izq;
 
     public NodoP() {
         izq = der = null;
     }
 
-    public String getNombre() {
-        return nombre;
+    public int getExt() {
+        return ext;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
+    public void setExt(int ext) {
+        this.ext = ext;
     }
 
     public NodoP getDer() {
