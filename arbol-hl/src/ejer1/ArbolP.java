62ec67f614e8917652969a9ff0bd415d77997337
John Castillo <john.gnu@gmail.com>
2022-11-25 13:12:12 -0400

    i4

--- arbol-hl/src/ejer1/ArbolP.java
+++ arbol-hl/src/ejer1/ArbolP.java
@@ -4,7 +4,39 @@ public class ArbolP {
 
     private NodoP raiz;
 
+    public NodoP getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NodoP raiz) {
+        this.raiz = raiz;
+    }
+
     ArbolP() {
         raiz = null;
     }
+
+    // aqui me falta a que metodo va este code
+    {
+        Cola niv = new Cola();
+        Cola desc = new Cola();
+        niv.adicionar(getRaiz());
+        while (!niv.esvacio()) {
+            while (!niv.esvacio()) {
+                NodoP r = niv.eliminar();
+
+                System.out.println("(" + r.getNom() + " " + r.getCon() + " " + r.getExt() + ")");
+                if (r.getIzq() != null) {
+                    desc.adicionar(r.getIzq());
+                }
+                if (r.getDer() != null) {
+                    desc.adicionar(r.getDer());
+                }
+
+            }
+            niv.vaciar(desc);
+            System.out.println("");
+        }
+    }
+
 }
--- arbol-hl/src/ejer1/NodoP.java
+++ arbol-hl/src/ejer1/NodoP.java
@@ -2,11 +2,28 @@ package ejer1;
 
 public class NodoP {
 
+    private String nom, con;
     private int ext;
     private NodoP der, izq;
 
     public NodoP() {
         izq = der = null;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCon() {
+        return con;
+    }
+
+    public void setCon(String con) {
+        this.con = con;
     }
 
     public int getExt() {
